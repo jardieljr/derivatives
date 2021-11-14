893ea99696f80c2dcfde321d4af7e1546906107d
Ilyes B <84982134+HnH19@users.noreply.github.com>
2021-11-14 18:38:48 +0000

    Update run_call_asian.py

--- assignment_2/Project/run_call_asian.py
+++ assignment_2/Project/run_call_asian.py
@@ -3,10 +3,14 @@ import asian_option_pricing as asp
 
 #Open a new excel
 
-book = ws.Workbook('../Derivatives_Project.xlsm')
+book = ws.Workbook('../Derivatives_Project_2.xlsm')
 
 sheet = book.sheets['Current Market Data']
 
 K= sheet['D5'].value
 
-asp.asian_option_pricing(K, option='call')
+price = asp.asian_option_pricing(K, option='call')
+
+sheet = book.sheets['MC']
+
+sheet['E2'].value = price
