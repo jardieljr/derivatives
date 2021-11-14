7841bbc667c6b78277559c8de0a972c2cc279600
Ilyes B <84982134+HnH19@users.noreply.github.com>
2021-11-14 18:13:46 +0000

    Update run_call_asian.py

--- assignment_2/Project/run_call_asian.py
+++ assignment_2/Project/run_call_asian.py
@@ -1,11 +1,12 @@
 import xlwings as ws
+import asian_option_pricing
 
 #Open a new excel
 
-book = ws.Workbook(../)
+book = ws.Workbook('../Derivatives_Project.xlsm')
 
-sheet = book.sheets[Current Market Data]
+sheet = book.sheets['Current Market Data']
 
-sheet['D5'].value = K
+K= sheet['D5'].value
 
 asian_option_pricing(K, option='call')
