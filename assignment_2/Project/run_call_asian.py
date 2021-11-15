efbebaa40f262845b0e82181ce94152210071b7a
Ilyes B <84982134+HnH19@users.noreply.github.com>
2021-11-15 00:02:42 +0000

    Update run_call_asian.py

--- assignment_2/Project/run_call_asian.py
+++ assignment_2/Project/run_call_asian.py
@@ -1,12 +1,11 @@
 import xlwings as ws
 import asian_option_pricing as asp
 
-import numpy as np
-
 #Open a new excel
 
-app = ws.App(visible=True)
-book = app.books.open('Derivatives_Project_2.xlsm')
+#app = ws.App(visible=False)
+#book = app.books.open('Derivatives_Project_2.xlsm')
+book = ws.Book('Derivatives_Project_2.xlsm')
 sheet = book.sheets['Current Market Data']
 
 K= sheet['D5'].value
@@ -16,3 +15,6 @@ price = asp.asian_option_pricing(K, option='call')
 sheet = book.sheets['MC']
 
 sheet['E2'].value = price
+
+book.save()
+book.close()
