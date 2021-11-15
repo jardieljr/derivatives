87208661eb70b61dc17bd027c22adb18dd26643c
Ilyes B <84982134+HnH19@users.noreply.github.com>
2021-11-15 22:05:39 +0000

    Update run_put_asian.py

--- assignment_2/Project/run_put_asian.py
+++ assignment_2/Project/run_put_asian.py
@@ -1,1 +1,20 @@
+import xlwings as ws
+import asian_option_pricing as asp
 
+#Open a new excel
+
+#app = ws.App(visible=False)
+#book = app.books.open('Derivatives_Project_2.xlsm')
+book = ws.Book('Derivatives_Project_2.xlsm')
+sheet = book.sheets['Current Market Data']
+
+K= sheet['D5'].value
+
+price = asp.asian_option_pricing(K, option='put')
+
+sheet = book.sheets['MC']
+
+sheet['E2'].value = price
+
+book.save()
+book.close()
