06a56430f27295f96b8bd28f44d9704993f0849a
jardieljr <39156622+jardieljr@users.noreply.github.com>
2021-11-16 20:44:55 +0100

    asian option model

--- assignment_2/Project/asian_option_pricing.py
+++ assignment_2/Project/asian_option_pricing.py
@@ -1,22 +1,42 @@
 import numpy as np
-from brownian_motion_simulation import *
+# from brownian_motion_simulation import *
 
-def asian_option_pricing(K, option='call'):
 
-    S = np.zeros((M + 1, I))    
-    S[0] = S0
+
+class AsianOption: 
+    def __init__(self, M, K, option_side, S0, sigma, T, r, I):
+        self.M = M
+        self.K = K
+        self.option=option_side
+        self.S0 = S0
+        self.sigma = sigma
+        self.T = T
+        self.r = r
+        self.I = I
+        self.dt = self.T/self.M
+        
+    def asian_option_pricing(self):
+
+        self.S = np.zeros((int(self.M + 1), int(self.I)))    
+        self.S[0] = self.S0
+        
+        for t in range(1, self.M + 1):
+            self.S[t] = self.S[t - 1] * np.exp((self.r - 0.5 * self.sigma ** 2) * self.dt 
+                    + self.sigma * np.sqrt(self.dt) * np.random.standard_normal(self.I))
+        
+        # calculate payoff at maturity
+        if self.option == 'Call':
+            payOff = np.maximum(self.S.mean(axis=0) - self.K, 0) # Average of Stock Price taken on path for each dt timestamp
+        else:
+            payOff = np.maximum(self.K - self.S.mean(axis=0), 0) # Average of Stock Price taken on path for each dt timestamp
+        
+        # calculate MCS estimator
+        optionPrice = np.exp(-self.r * self.T) * 1 / self.I * np.sum(payOff)
+        
+        return optionPrice
     
-    for t in range(1, M + 1):
-        S[t] = S[t - 1] * np.exp((r - 0.5 * sigma ** 2) * dt 
-                + sigma * np.sqrt(dt) * np.random.standard_normal(I))
+if __name__ == '__main__':
     
-    # calculate payoff at maturity
-    if option == 'call':
-        payOff = np.maximum(S.mean(axis=0) - K, 0) # Average of Stock Price taken on path for each dt timestamp
-    else:
-        payOff = np.maximum(K - S.mean(axis=0), 0) # Average of Stock Price taken on path for each dt timestamp
+    x = AsianOption(100, 105, 'Call', 105, 0.2, 1, 0.01, 100000)
     
-    # calculate MCS estimator
-    optionPrice = np.exp(-r * T) * 1 / I * np.sum(payOff )
-    
-    return optionPrice
+    result = x.asian_option_pricing()
